d9305ce41bb2fbbbfb2a9509fef4ec43550690d7
Megan Karbowski <mck064@gmail.com>
2020-11-05 16:01:00 -0800

    removed nav container

--- mkarbowski/src/app/home-page/home-page.module.ts
+++ mkarbowski/src/app/home-page/home-page.module.ts
@@ -7,14 +7,12 @@ import { MaterialModule } from '../material/material.module';
 import { HomePageRoutingModule } from './home-page-routing.module';
 
 // components
-import { NavigationContainerComponent } from './containers/navigation-container/navigation-container.component';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { HomePageContainerComponent } from './containers/home-page-container/home-page-container.component';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 
 @NgModule({
   declarations: [
-    NavigationContainerComponent,
     NavigationComponent,
     HomePageContainerComponent,
     AboutPageComponent
